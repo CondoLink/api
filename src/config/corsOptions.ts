4b89e7008b876fe10ed9f190a6381bb1137e2bd8
Dalmir Pereira <dalmir_pereira@outlook.com>
2025-07-10 18:37:02 +1000

    updated whitelist and cors (frontend access)

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,13 +1,12 @@
-import { CorsOptions } from 'cors';
-
-const whitelist = require('./whitelist');
+import { CorsOptions } from "cors";
+const whitelist = require("./whitelist");
 
 const corsOptions: CorsOptions = {
   origin: (origin: any, callback: (err: Error | null, allow?: boolean) => void) => {
-    if (origin === undefined || whitelist.indexOf(origin) !== -1) {
+    if (origin === undefined || whitelist.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
--- src/config/whitelist.ts
+++ src/config/whitelist.ts
@@ -1,5 +1,5 @@
-const whitelist = [
-    'http://localhost:5173'
-];
+const whitelist = process.env.CORS_WHITELIST
+  ? process.env.CORS_WHITELIST.split(",").map((url) => url.trim())
+  : [];
 
 module.exports = whitelist;
